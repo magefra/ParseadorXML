882a1e44d25d95a4fa3691778fb6f84cc2e24646
Ing. Magdiel Efrain Palacios Rivera <magdiel.palacios@csfacturacion.com>
2020-03-05 17:41:39 -0600

    proyecto
    Agregando proyecto

---
+++ ParseadorXML/ParseadorXML.Core/src/Entities/CFDI/Complementos/TimbreFiscal/TimbreFiscal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseadorXML.Core.src.Entities.CFDI.Complementos.TimbreFiscal
+{
+    public class TimbreFiscal
+    {
+    }
+}
