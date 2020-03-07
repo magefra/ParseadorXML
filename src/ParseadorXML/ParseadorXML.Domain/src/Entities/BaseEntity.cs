c81910b7094b70365969a263edce1f0d0158a631
Ing. Magdiel Efrain Palacios Rivera <magdiel.palacios@csfacturacion.com>
2020-03-07 01:55:15 -0600

    Agregando estructura del proyecto.

---
+++ src/ParseadorXML/ParseadorXML.Domain/src/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParseadorXML.Domain.src.Entities
+{
+    public abstract  class BaseEntity
+    {
+
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public virtual Guid Id { get; set; }
+    }
+}
---
+++ src/ParseadorXML/ParseadorXML.Domain/src/Entities/CFDI/Comprobante/Receptor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParseadorXML.Domain.src.Entities.CFDI.Comprobante
+{
+    class Receptor
+    {
+    }
+}
